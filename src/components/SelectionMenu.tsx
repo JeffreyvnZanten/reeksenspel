86ca598269200d3766bf8ecc90ed134e711565bf
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-02-15 19:06:48 +0100

    Letters of nummers keuze

--- src/components/SelectionMenu.tsx
+++ src/components/SelectionMenu.tsx
@@ -1,34 +1,54 @@
-import { generateNumberSequence } from "../utils";
+import { generateLetterSequence, generateNumberSequence } from "../utils";
 import { CustomSettings } from "./CustomSettings";
 
 export type AllowedSequenceItem = number | string | HTMLImageElement;
+
+
 
 export type SelectionMenuProps<T> = {
   setGameState: () => void;
   setSequence: React.Dispatch<React.SetStateAction<T[] | null>>;
 };
 
-
 export const SelectionMenu = <T,>({setGameState, setSequence}: SelectionMenuProps<T>) => {
+
+  function handleSequenceChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    
+    switch (value) {
+      case "nummers":
+        setSequence(generateNumberSequence(2, 3) as T[]);
+        break;
+      case "letters":
+        setSequence(generateLetterSequence(4) as T[]);
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <div className="text-center">
         <h1 className="text-2xl">Selection Menu</h1>
-        <CustomSettings />
-        <button 
-            className="bg-red-500 hover:bg-red-700 text-xl text-white font-bold py-1 px-6 rounded-lg"
-            onClick={() => setSequence(generateNumberSequence(2, 3) as T[])}
-        >10 tm 99
-        </button>
-        <button 
-            className="bg-red-500 hover:bg-red-700 text-xl text-white font-bold py-1 px-6 rounded-lg"
-            onClick={() => setSequence(generateNumberSequence(3, 3) as T[])}
-        >100 tm 999
-        </button>
-        <button 
-            className="bg-red-500 hover:bg-red-700 text-xl text-white font-bold py-1 px-6 rounded-lg"
-            onClick={() => setSequence(generateNumberSequence(4, 3) as T[])}
-        >1000 tm 9999
-        </button>
+        <fieldset>
+          <legend>Type Reeksen</legend>
+          <input 
+            type="radio" 
+            id="nummers" 
+            name="reeksen" 
+            value="nummers" 
+            onChange={handleSequenceChange} 
+          />
+          <label htmlFor ="nummers">Nummers</label>
+          <input 
+            type="radio" 
+            id="letters" 
+            name="reeksen" 
+            value="letters" 
+            onChange={handleSequenceChange} 
+          />
+          <label htmlFor ="letters">Letters</label>
+        </fieldset>
         <button 
             className="bg-red-500 hover:bg-red-700 text-xl text-white font-bold py-1 px-6 rounded-lg"
             onClick={() => setGameState()}
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,12 @@
 export function generateNumberSequence(digits: number, repeatAmount: number): number[] {
-    const min = Math.pow(10, digits - 1);
-    const max = Math.pow(10, digits) - 1;
+  const min = Math.pow(10, digits - 1);
+  const max = Math.pow(10, digits) - 1;
 
-    return Array.from({ length: repeatAmount }, () => Math.floor(Math.random() * (max - min + 1)) + min);
-  }
+  return Array.from({ length: repeatAmount }, () => Math.floor(Math.random() * (max - min + 1)) + min);
+}
+
+export function generateLetterSequence(repeatAmount: number): string[] {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  return Array.from({ length: repeatAmount }, () => alphabet[Math.floor(Math.random() * alphabet.length)]);
+}
   
